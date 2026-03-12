3342d202a17921ba0c6780a388997ac3ac3d0267
EscanorX144 <contisarto@gmail.com>
2026-03-12 09:44:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ db = client['kiwii_game_shop']
 orders_col = db['orders']
 
 # --- ⚙️ CONFIGURATION ---
-BOT_TOKEN = "8089066962:AAFOHBGeuDF7E3Ygej3nAwOODsNj4ujVvk"
+BOT_TOKEN = "8089066962:AAFOHBGeuDF7E3YgeJ3mUu000sQNJ4uJVok"
 CHAT_ID = "7089720301"
 CS_LINK = "https://t.me/Bby_kiwii7"
 
