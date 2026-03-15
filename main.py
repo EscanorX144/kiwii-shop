49772d3036d7344102e610aa72a5d9ef94fface2
EscanorX144 <contisarto@gmail.com>
2026-03-15 11:35:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,8 @@ GAMES_DATA = [
             ]
         }
     },
-   [
+ [
+ 
 HTML_CODE = '''
 <!DOCTYPE html><html><head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
